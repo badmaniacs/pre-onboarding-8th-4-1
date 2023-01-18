c05cfedd5f7620575f40c6cdf479ecb936604c73
KyungTae Park <96967183+badmaniacs@users.noreply.github.com>
2023-01-18 20:48:06 +0900

    ✨ Feat :  참고용 코드를 통한 컴포넌트 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import React from "react";
+import CommentListContainer from "./containers/CommentListContainer";
+import PageListContainer from "./containers/PageListContainer";
+import FormContainer from "./containers/FormContainer";
+
 const App = () => {
-  return <div>boilerplate</div>;
-};
+  return (
+    <div>
+      <CommentListContainer />
+      <PageListContainer />
+      <FormContainer />
+    </div>
+  );
+}
 
 export default App;
