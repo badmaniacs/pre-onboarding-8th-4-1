8522e9e25007c6d878d72cb639f525185a8da405
Hyejung Park <hyejj19@naver.com>
2023-01-18 16:55:33 +0900

    🔨 Modify:  API return 값 수정

--- src/api/commentsAPI.ts
+++ src/api/commentsAPI.ts
@@ -3,15 +3,15 @@ import { httpClient } from '../utils/httpClient';
 
 export const commentsAPI = {
   get: (url: string) => {
-    return httpClient.get(url).then((res) => res.data);
+    return httpClient.get(url);
   },
   post: (url: string, comment: Comment) => {
-    return httpClient.post(url, comment).then((res) => res.data);
+    return httpClient.post(url, comment);
   },
   update: (url: string, comment: CommentUpdate) => {
-    return httpClient.patch(url, comment).then((res) => res.data);
+    return httpClient.patch(url, comment);
   },
   delete: (url: string) => {
-    return httpClient.delete(url).then((res) => res.data);
+    return httpClient.delete(url);
   },
 };
