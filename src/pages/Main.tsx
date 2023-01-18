198f7cd883a25eac0ee5616defbd42a4d90ca89e
Hyejung Park <hyejj19@naver.com>
2023-01-18 23:52:09 +0900

    💄 Design:  width 수정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,21 @@
+import styled from 'styled-components';
 import CommentList from '../features/comments/CommentList';
 import PageList from '../features/comments/PageList';
 import Form from '../features/form/Form';
 
 const Main = () => {
   return (
-    <>
+    <Wrapper>
       <CommentList />
       <PageList />
       <Form />
-    </>
+    </Wrapper>
   );
 };
 
 export default Main;
+
+const Wrapper = styled.div`
+  width: 500px;
+  height: 100vh;
+`;
